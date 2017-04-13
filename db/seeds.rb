72c858c54061e0215af1159b3a9ce24cdc7b7667
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-04-13 16:27:37 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@team_amazing = Team.create(name: 'Team Amazing')
+
+@admin = User.create(
+  name: 'Admin',
+  email: 'admin@amazing.com',
+  admin: true,
+  team: @team_amazing
+)
+
+@user = User.create(
+  name: 'John Smart',
+  email: 'john.smart@amazing.com',
+  team: @team_amazing
+)
+
+[
+  {
+    name: 'GCAM. Global Change Assessment Model'
+  },
+  {
+    name: 'EPS. Energy Policy Solutions Model'
+  },
+  {
+    name: 'E3 Pathways Model'
+  }
+].each { |model_attrs| Model.create(model_attrs.merge(team: @team_amazing))}
