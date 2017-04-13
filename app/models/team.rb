a751e01a5a2fc568b2b7862fa875e60497616073
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-04-13 16:44:41 +0200

    Specs for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
   has_many :users, dependent: :restrict_with_error
   has_many :models, dependent: :restrict_with_error
+
+  validates :name, presence: true, uniqueness: true
 end
---
+++ spec/models/team_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Team, type: :model do
+  it "should be invalid when name not present" do
+    expect(
+      FactoryGirl.build(:team, name: nil)
+    ).to have(1).errors_on(:name)
+  end
+  it "should be invalid when name not unique" do
+    FactoryGirl.create(:team, name: 'A-Team')
+    expect(
+      FactoryGirl.build(:team, name: 'A-Team')
+    ).to have(1).errors_on(:name)
+  end
+end
