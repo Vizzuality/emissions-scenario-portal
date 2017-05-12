afa4896f0e8ea147c5bec1ed2088ff46251fe729
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-12 12:03:57 +0100

    Added routes for upload actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :models, only: [:index, :show, :edit, :update] do
-    resources :scenarios, only: [:index], shallow: true
+    post :upload_meta_data, on: :collection
+    resources :scenarios, only: [:index], shallow: true do
+      post :upload_meta_data, on: :collection
+      post :upload_time_series, on: :collection, to: 'time_series_values#upload', as: :upload_time_series
+    end
   end
-  resources :indicators, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :indicators, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    post :upload_meta_data, on: :collection#, as: :upload_indicators_meta_data
+  end
 
   root to: 'models#index'
 end
