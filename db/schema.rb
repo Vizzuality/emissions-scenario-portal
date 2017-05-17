2a55b2773fc3f91ce9f28b4cd61450aff4008ea2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-17 12:33:45 +0100

    Added not null constaint for scenario name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517104211) do
+ActiveRecord::Schema.define(version: 20170517113111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 20170517104211) do
   end
 
   create_table "scenarios", force: :cascade do |t|
-    t.text     "name"
+    t.text     "name",                                          null: false
     t.integer  "model_id"
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
