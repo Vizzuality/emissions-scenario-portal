261067cc6147dd16b38e1044cbb6cce77ecd4349
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-17 10:05:39 +0100

    Values coming from time series data

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,4 +1,14 @@
 class Scenario < ApplicationRecord
   belongs_to :model
   has_many :time_series_values, dependent: :destroy
+  has_many :indicators, through: :time_series_values
+
+  def meta_data?
+    # TODO: what to check here?
+    true
+  end
+
+  def time_series_data?
+    time_series_values.any?
+  end
 end
