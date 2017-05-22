2ee9af98d99a5fe1d77a733833ac779e8045c9f1
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-22 11:45:02 +0100

    Require authentication for all actions

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,7 +1,7 @@
 module ControllerMacros
   def login_admin
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       @admin = FactoryGirl.create(:user, admin: true)
       sign_in @admin
     end
@@ -9,7 +9,7 @@ module ControllerMacros
 
   def login_user
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       @user = FactoryGirl.create(:user)
       sign_in @user
     end
