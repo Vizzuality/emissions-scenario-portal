c6c17fec9edcef9091c06e13e59dbe801963d66e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-22 12:54:32 +0100

    Added devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
-    :validatable
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable,
+         :trackable, :validatable
   belongs_to :team, optional: true
 
   validates :team, presence: true, unless: :admin?
