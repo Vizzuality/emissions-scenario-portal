3064d0d10d423f9cd74384d7522a1bcba3132079
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-22 11:44:01 +0100

    Removed registerable & confirmable modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+    :validatable
   belongs_to :team, optional: true
 
   validates :team, presence: true, unless: :admin?
-  # TODO: format validation: to be decided when registration / auth style
-  # determined
 end
