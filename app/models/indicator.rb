5e520d15090742688813bf3316388f710140ee43
pjosh <jorgegar@gmail.com>
2017-05-24 12:48:58 +0200

    Case insensitive when filter by name

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -41,7 +41,7 @@ class Indicator < ApplicationRecord
 
       case filter
       when 'search'
-        indicators.where("name LIKE '%#{value}%'")
+        indicators.where("lower(name) LIKE '%#{value.downcase}%'")
       when 'order_type'
         fetch_with_order(
           indicators,
