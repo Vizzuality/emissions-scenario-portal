d46cb223482e342daf749740961160f5fdf70c17
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-26 10:06:05 +0100

    Changed json reponses into redirects with flash messages.

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -2,29 +2,49 @@ class TeamUsersController < ApplicationController
   before_action :set_team
 
   def create
-    email = user_params[:email]
-    @user = User.where('LOWER(email) = ?', email.downcase.strip).first
-    if @user.nil?
-      @user = User.invite!(email: email, team: @team)
-    else
-      @user.update_attributes(team: @team)
-    end
-
-    if @user.errors.empty?
-      render json: @user
-    else
-      render json: {errors: @user.errors}, status: :unprocessable_entity
-    end
+    email = user_params[:email].downcase.strip
+    @user = User.where('LOWER(email) = ?', email).first
+    invite_user(email) and return if @user.nil?
+    @user.update_attributes(team: @team)
+    flash_message =
+      if @user.errors.empty?
+        {notice: 'User successfully added to team.'}
+      else
+        {
+          alert: "User could not be added to team. Please ensure this user \
+            is not a member of another team."
+        }
+      end
+    redirect_to edit_team_url(@team), flash_message
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.team_id = nil
     @user.save(validate: false)
-    render json: @person, status: :ok
+    redirect_to(
+      edit_team_url(@team),
+      notice: 'User successfully removed from team.'
+    )
   end
 
   private
+
+  def invite_user(email)
+    user = User.new(email: email, team: @team)
+    user.valid?
+    flash_message =
+      if user.errors[:email].any?
+        {
+          alert: "User could not be invited. Please ensure email address is \
+            valid."
+        }
+      else
+        @user = User.invite!(email: email, team: @team)
+        {notice: 'User successfully invited to team.'}
+      end
+    redirect_to edit_team_url(@team), flash_message
+  end
 
   def set_team
     @team = Team.find(params[:team_id])
