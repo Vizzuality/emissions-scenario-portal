4d9fa07eebe81538b8c9b2582ceda4c1db279787
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-30 14:15:47 +0100

    Simplified file system operations

--- app/services/upload_time_series_values.rb
+++ app/services/upload_time_series_values.rb
@@ -6,29 +6,12 @@ class UploadTimeSeriesValues
   end
 
   def call(uploaded_io)
-    store_file(uploaded_io) # TODO: record the fact of the upload?
-    begin
-      data = TimeSeriesValuesData.new(@path)
-      data.process
-    ensure
-      discard_file
-    end
+    data = TimeSeriesValuesData.new(uploaded_io.tempfile)
+    data.process
     FileUploadStatus.new(
       data.number_of_rows,
       data.number_of_rows_failed,
       data.errors
     )
-  end
-
-  def store_file(uploaded_io)
-    @filename = "#{Time.now.getutc}-#{uploaded_io.original_filename}"
-    @path = Rails.root.join('public', 'uploads', @filename)
-    File.open(@path, 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
-  end
-
-  def discard_file
-    FileUtils.rm(@path)
   end
 end
