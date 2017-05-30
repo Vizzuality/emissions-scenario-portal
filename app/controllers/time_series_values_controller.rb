25d7766d01fe1927b19158791993bd0e7ebc24ec
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-05-30 14:04:30 +0100

    Check if upload file provided

--- app/controllers/time_series_values_controller.rb
+++ app/controllers/time_series_values_controller.rb
@@ -2,8 +2,9 @@ class TimeSeriesValuesController < ApplicationController
   def upload
     @model = Model.find(params[:model_id])
     @uploaded_io = params[:time_series_values_file]
-    # TODO: pass current user into service object
-    result = UploadTimeSeriesValues.new(nil, @model).call(@uploaded_io)
+    redirect_to model_scenarios_url(@model),
+      alert: 'Please provide an upload file' and return unless @uploaded_io.present?
+    result = UploadTimeSeriesValues.new(current_user, @model).call(@uploaded_io)
     render json: result
   end
 end
