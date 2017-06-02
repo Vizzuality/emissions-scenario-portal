32887eb23b7466d92ebb2205ff477cdc5b1273fd
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-06-02 13:42:42 +0100

    Processing scenarios upload file

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -9,6 +9,7 @@ class Scenario < ApplicationRecord
   has_many :indicators, through: :time_series_values
 
   validates :name, presence: true
+  validates :model, presence: true
   # TODO: validate release date is a date
   before_validation :ignore_blank_array_values
 
