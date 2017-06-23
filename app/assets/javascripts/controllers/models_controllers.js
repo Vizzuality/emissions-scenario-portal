77eb8b1efdb41e4556a2db092d574401495fc9bc
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-06-23 12:34:15 +0100

    Added upload button

--- app/assets/javascripts/controllers/models_controllers.js
+++ app/assets/javascripts/controllers/models_controllers.js
@@ -6,7 +6,13 @@
 
   App.Controller.Models = App.Controller.Page.extend({
 
-    index: function () { },
+    index: function () {
+      _.each($('.js-upload-card'), function(element) {
+        new App.Helper.InputFileUploader({
+          el: element
+        });
+      }.bind(this));
+    },
 
     show: function () {
       new App.View.FormVerticalNav();
