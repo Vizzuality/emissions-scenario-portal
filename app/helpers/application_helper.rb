c96a9bedbab297aeae4cfd2876506031ba2cce73
pjosh <jorgegar@gmail.com>
2017-07-03 16:06:37 +0200

    Hound desires

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,14 +72,21 @@ module ApplicationHelper
 
   def checkbox_input(_object, form, attr_info)
     content_tag :div, class: 'c-checkbox' do
-      concat(form.check_box attr_info.name)
+      concat((form.check_box attr_info.name))
       concat(form.label(attr_info.name, '') do
         content_tag :div, class: 'c-checkbox__box' do
-          content_tag(:svg, content_tag(:use, '', { 'xlink:href' => '#icon-checkbox-off' }), class: 'icon icon-checkbox-off') +
-          content_tag(:svg, content_tag(:use, '', { 'xlink:href' => '#icon-checkbox-on' }), class: 'icon icon-checkbox-on')
+          svg('icon-checkbox-off') + svg('icon-checkbox-on')
         end
       end)
     end
+  end
+
+  def svg(id)
+    content_tag(
+      :svg,
+      content_tag(:use, '', 'xlink:href' => "##{id}"),
+      class: "icon #{id}"
+    )
   end
 
   def values_for_attribute_dropdown(object, attr_info)
