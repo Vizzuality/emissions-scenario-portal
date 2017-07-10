0d44b14688c2b458c5382b746f4b85d0ed3a3068
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-07-10 13:32:19 +0100

    Added down migration

--- db/migrate/20170710093733_change_model_id_to_team_id_in_indicators.rb
+++ db/migrate/20170710093733_change_model_id_to_team_id_in_indicators.rb
@@ -1,7 +1,16 @@
 class ChangeModelIdToTeamIdInIndicators < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_reference :indicators, :team, index: true, foreign_key: true
     execute 'UPDATE indicators SET team_id = models.team_id FROM models WHERE models.id = indicators.model_id'
     remove_column :indicators, :model_id
+  end
+
+  def down
+    add_reference :indicators, :model, index: true, foreign_key: true
+    Indicator.where('team_id IS NOT NULL').each do |i|
+      i.model_id = i.team.models.first.try(:id)
+      i.save
+    end
+    remove_column :indicators, :team_id
   end
 end
