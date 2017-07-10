6e2888cd92b630424e1f8239b34c116e478bc245
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-07-10 09:27:58 +0100

    Parent indicator dropdown

--- app/helpers/indicators_helper.rb
+++ app/helpers/indicators_helper.rb
@@ -18,6 +18,14 @@ module IndicatorsHelper
     )
   end
 
+  def values_for_indicator_parent_dropdown(indicator)
+    selection = indicator.parent
+    select_values = Indicator.where('model_id IS NULL').select(:id, :alias).map do |i|
+      [i.alias, i.id]
+    end
+    [select_values, selection]
+  end
+
   def destroy_confirmation_message(indicator)
     time_series_data = 'Time series data exists for this indicator. '
     message = 'Are you sure you want to proceed?'
