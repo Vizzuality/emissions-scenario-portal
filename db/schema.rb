5c59802045c361b91f0b2ca3fd00601003c78dea
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-07-21 13:02:02 +0100

    Re-added iso code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713095925) do
+ActiveRecord::Schema.define(version: 20170721104917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170713095925) do
     t.boolean  "region",     default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "iso_code"
   end
 
   create_table "models", force: :cascade do |t|
