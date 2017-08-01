e7d8a2a51d35ab44092492360bdce7ccf221273d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-08-01 11:14:18 +0200

    Allow model indicator name header without model abbreviation

--- lib/modules/indicators_headers.rb
+++ lib/modules/indicators_headers.rb
@@ -59,7 +59,12 @@ class IndicatorsHeaders
     @actual_headers = @headers.
       map { |ah| ah.downcase.gsub(/[^a-z0-9]/i, '') }.
       map do |header|
-      expected_index = expected_headers.index(header)
+      expected_index = if header == 'modelindicatorname'
+        # this is in case somebody has not amended the model name header
+        expected_headers.index("#{@model.abbreviation.downcase}indicatorname")
+      else
+        expected_headers.index(header)
+      end
       if expected_index.present?
         {
           display_name: header,
