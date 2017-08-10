4613c402890c136b0338cff2a14dc8cc64ac45dd
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-08-10 11:14:40 +0100

    Changed scope of controller tests

--- spec/controllers/api/v1/models_controller_spec.rb
+++ spec/controllers/api/v1/models_controller_spec.rb
@@ -1,20 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::ModelsController, type: :controller do
+describe Api::V1::ModelsController, type: :controller do
   context do
     let!(:some_models) { FactoryGirl.create_list(:model, 3) }
 
     describe 'GET index' do
+      it 'returns a successful 200 response' do
+        get :index
+        expect(response).to be_success
+      end
+
       it 'lists all models' do
         get :index
-        expect(response.body).to eq(some_models.sort_by(&:full_name).to_json)
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.length).to eq(3)
       end
     end
 
     describe 'GET show' do
+      it 'returns a successful 200 response' do
+        get :show, params: {id: some_models[0].id}
+        expect(response).to be_success
+      end
+
       it 'shows one model' do
         get :show, params: {id: some_models[0].id}
-        expect(response.parsed_body).to eq(some_models[0].to_json)
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body).to_not be_nil
       end
     end
   end
--- spec/controllers/api/v1/scenarios_controller_spec.rb
+++ spec/controllers/api/v1/scenarios_controller_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::ScenariosController, type: :controller do
+describe Api::V1::ScenariosController, type: :controller do
   context do
     let!(:some_model) { FactoryGirl.create(:model) }
     let!(:some_scenarios) { FactoryGirl.create_list(:scenario, 3, model: some_model) }
 
     describe 'GET index' do
+      it 'returns a successful 200 response' do
+        get :index,  params: {model_id: some_model.id}
+        expect(response).to be_success
+      end
+
       it 'lists all scenarios that belong to one model' do
         get :index, params: {model_id: some_model.id}
-        expect(response.body).to eq(some_scenarios.sort_by(&:name).to_json)
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.length).to eq(3)
       end
     end
   end
