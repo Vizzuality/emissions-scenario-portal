999b0434b60deb68539b4c73212a67010e0d36ff
Kuba Kuźma <kuba@jah.pl>
2017-11-03 09:00:16 +0100

    fixed styling

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -47,8 +47,8 @@ class ModelsController < ApplicationController
   def show
     @scenarios = @model.scenarios.limit(5)
     @indicators = Indicator.
-                    system_indicators_with_variations.
-                    order(:category, :subcategory, :name)
+      system_indicators_with_variations.
+      order(:category, :subcategory, :name)
   end
 
   def destroy
