529acccc59d84698f9ba731ca9ec15e180c6ace0
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-11-08 18:22:56 +0000

    Add cors headers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class ApiController < ActionController::API
+      before_action :set_access_control_headers
+
+      def set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'GET'
+      end
     end
   end
 end
