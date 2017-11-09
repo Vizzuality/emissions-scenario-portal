1793b5aafec21410a0483acb2c8af869bf71a4bf
Simao Belchior <simaobelchior@gmail.com>
2017-11-09 16:18:29 +0100

    Adds missing end removed while merging conflicts...sorry

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ApiController < ActionController::API
-      
+
       rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
 
       def resource_not_found
@@ -9,12 +9,13 @@ module Api
           code: 404,
           status: 'resource not found'
         }, status: :not_found
-        
-      before_action :set_access_control_headers
 
-      def set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'GET'
+        before_action :set_access_control_headers
+
+        def set_access_control_headers
+          headers['Access-Control-Allow-Origin'] = '*'
+          headers['Access-Control-Allow-Methods'] = 'GET'
+        end
       end
     end
   end
