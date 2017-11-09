0863aab8a1e4b09c85568cfa447c5bddeb29a5de
Kuba Kuźma <kuba@jah.pl>
2017-11-09 10:22:54 +0100

    fixed some hound complaints

--- app/services/filter_scenarios.rb
+++ app/services/filter_scenarios.rb
@@ -3,12 +3,21 @@ class FilterScenarios
 
   include ActiveModel::Model
 
-  attr_accessor :search, :order_type, :order_direction
+  attr_writer :order_type, :order_direction
+  attr_accessor :search
 
   def call(scope)
     scope.
       merge(search_scope).
       merge(order_scope)
+  end
+
+  def order_type
+    @order_type if ORDER_COLUMNS.include?(@order_type)
+  end
+
+  def order_direction
+    @order_direction.to_s.casecmp('desc').zero? ? :desc : :asc
   end
 
   private
@@ -19,12 +28,12 @@ class FilterScenarios
 
   def search_scope
     return scenarios if search.blank?
+
     scenarios.where('lower(name) LIKE ?', "%#{search.downcase}%")
   end
 
   def order_scope
-    return scenarios unless ORDER_COLUMNS.include?(order_type)
-    direction = order_direction.to_s.casecmp('desc').zero? ? :desc : :asc
+    return scenarios if order_type.blank?
 
     case order_type
     when 'indicators'
--- spec/services/filter_scenarios_spec.rb
+++ spec/services/filter_scenarios_spec.rb
@@ -3,54 +3,54 @@ require 'rails_helper'
 RSpec.describe FilterScenarios do
   context 'when using search' do
     it 'filters by category' do
-      high_scenario_1 = create(:scenario, name: 'High 1')
-      high_scenario_2 = create(:scenario, name: 'High 2')
+      high1 = create(:scenario, name: 'High 1')
+      high2 = create(:scenario, name: 'High 2')
       create(:scenario, name: 'Low')
 
       expect(
         FilterScenarios.
           new(search: 'high').
           call(Scenario.all)
-      ).to match_array([high_scenario_1, high_scenario_2])
+      ).to match_array([high1, high2])
     end
   end
 
   context 'when sorting' do
     it 'orders by name' do
-      high_scenario_1 = create(:scenario, name: 'High 1')
-      high_scenario_2 = create(:scenario, name: 'High 2')
+      high1 = create(:scenario, name: 'High 1')
+      high2 = create(:scenario, name: 'High 2')
 
       expect(
         FilterScenarios.
           new(order_type: 'name', order_direction: 'desc').
           call(Scenario.all)
-      ).to eq([high_scenario_2, high_scenario_1])
+      ).to eq([high2, high1])
     end
 
     it 'orders by number of indicators' do
-      with_2 = create(:scenario, name: 'with 2')
-      create_list(:time_series_value, 2, scenario: with_2)
+      with2 = create(:scenario, name: 'with 2')
+      create_list(:time_series_value, 2, scenario: with2)
       without = create(:scenario, name: 'without')
-      with_3 = create(:scenario, name: 'with 3')
-      create_list(:time_series_value, 3, scenario: with_3)
+      with3 = create(:scenario, name: 'with 3')
+      create_list(:time_series_value, 3, scenario: with3)
 
       expect(
         FilterScenarios.
           new(order_type: 'indicators', order_direction: 'asc').
           call(Scenario.all)
-      ).to eq([without, with_2, with_3])
+      ).to eq([without, with2, with3])
     end
 
     it 'orders by time series' do
-      with_2 = create(:scenario, name: 'with 2')
-      create_list(:time_series_value, 2, scenario: with_2)
+      with2 = create(:scenario, name: 'with 2')
+      create_list(:time_series_value, 2, scenario: with2)
       without = create(:scenario, name: 'without')
 
       expect(
         FilterScenarios.
           new(order_type: 'time_series', order_direction: 'asc').
           call(Scenario.all)
-      ).to eq([without, with_2])
+      ).to eq([without, with2])
     end
   end
 end
