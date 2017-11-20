8cfef434be601a8e362456c1d71d871256318b5d
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-11-20 15:02:17 +0000

    Fix code style

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,9 +2,7 @@ module Api
   module V1
     class CategoriesController < ApiController
       def index
-        categories = Category.
-          order(:name).
-          all
+        categories = Category.order(:name).all
 
         render json: categories
       end
