986310330adafeb57ac0b05b12003a3cb5492efe
Tiago Santos <santos.tiago@gmail.com>
2017-11-22 19:03:12 +0100

    Added country filter to the model endpoint

--- app/controllers/api/v1/models_controller.rb
+++ app/controllers/api/v1/models_controller.rb
@@ -2,10 +2,19 @@ module Api
   module V1
     class ModelsController < ApiController
       def index
-        models = Model.
-          includes(:scenarios, :indicators).
-          order(:full_name)
+        if location_ids.blank?
+          models = Model.
+              includes(:scenarios, :indicators).
+              order(:full_name)
+        else
+          models = Model.
+            joins({indicators: {time_series_values: :location}}, :scenarios).
+            where(indicators: {time_series_values: {location_id: location_ids}}).
+            order(:full_name).distinct
+        end
 
+        puts models.count
+        models.each {|x| puts x.id}
         render json: models
       end
 
@@ -15,6 +24,16 @@ module Api
           find_by!(id: params[:id])
 
         render json: model
+      end
+
+      private
+
+      def location_ids
+        if params[:location].blank?
+          nil
+        else
+          params[:location].split(',')
+        end
       end
     end
   end
