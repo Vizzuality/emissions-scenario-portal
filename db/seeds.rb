9bef53f17a2112cb94c2d3d0f7d7a264a5aeec90
Kuba Kuźma <kuba@jah.pl>
2017-11-23 14:34:41 +0100

    simplified upcasing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 load Rails.root.join('db', 'seeds', 'regions.rb')
 load Rails.root.join('db', 'seeds', 'locations.rb')
-load Rails.root.join("db", "seeds", "#{Rails.env}.rb")
+load Rails.root.join('db', 'seeds', "#{Rails.env}.rb")
