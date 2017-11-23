ea29af2887e81f0e2bd05e6cef621c3052bca12f
Kuba Kuźma <kuba@jah.pl>
2017-11-23 14:00:54 +0100

    fixed specs, upcasing iso codes

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,12 @@ class Location < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :iso_code, length: {is: 2}, uniqueness: true, unless: :region?
   validates :region, inclusion: {in: [false, true]}
+
+  before_validation :upcase_iso_code
+
+  private
+
+  def upcase_iso_code
+    iso_code.upcase!
+  end
 end
