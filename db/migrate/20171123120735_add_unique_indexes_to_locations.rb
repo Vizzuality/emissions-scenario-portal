48e1761affc70fc2e0afddc27dedbe3e8336a1a7
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-11-23 16:07:28 +0000

    Fix 20171123120735_add_unique_indexes_to_locations
    This migration created an unique index over name and iso_code on the locations table,
    but didn't contemplate the fact that there are several rows without iso_codes in the
    production database (these rows refer to regions).
    
    This fix does two things:
    - converts empty iso strings to nulls
    - changes the unique index on iso_code to be scoped to non-null values

--- db/migrate/20171123120735_add_unique_indexes_to_locations.rb
+++ db/migrate/20171123120735_add_unique_indexes_to_locations.rb
@@ -1,6 +1,10 @@
 class AddUniqueIndexesToLocations < ActiveRecord::Migration[5.1]
+  class Location < ApplicationRecord
+  end
+
   def change
+    Location.where(iso_code: '').update(iso_code: nil)
     add_index :locations, :name, unique: true
-    add_index :locations, :iso_code, unique: true
+    add_index :locations, :iso_code, unique: true, where: 'iso_code IS NOT NULL'
   end
 end
