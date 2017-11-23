55fc32df79a71a9fae4269f6d76dfdc9835eb198
Kuba Kuźma <kuba@jah.pl>
2017-11-23 13:21:59 +0100

    moved common seeds to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-load Rails.root.join("db", "seeds", "common.rb")
+load Rails.root.join('db', 'seeds', 'regions.rb')
+load Rails.root.join('db', 'seeds', 'locations.rb')
 load Rails.root.join("db", "seeds", "#{Rails.env}.rb")
--- db/seeds/common.rb
+++
@@ -1,2 +0,0 @@
-load Rails.root.join('db', 'seeds', 'regions.rb')
-load Rails.root.join('db', 'seeds', 'locations.rb')
