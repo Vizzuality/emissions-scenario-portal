e7ceb5347624e397323fd2c6af4a048930410825
Simao Belchior <simaobelchior@gmail.com>
2017-12-05 12:58:41 +0000

    Filter by model should go through scenario. Adds specs

--- spec/controllers/api/v1/time_series_values_controller_spec.rb
+++ spec/controllers/api/v1/time_series_values_controller_spec.rb
@@ -14,6 +14,18 @@ describe Api::V1::TimeSeriesValuesController, type: :controller do
         scenario: scenario
       )
     }
+    let!(:model_with_time_series) {
+      model = create(:model)
+      my_scenario = create(:scenario, model_id: model.id)
+      create_list(
+        :time_series_value,
+        5,
+        location: location,
+        indicator: indicator,
+        scenario: my_scenario
+      )
+      model
+    }
 
     describe 'GET index' do
       it 'returns a successful 200 response' do
@@ -24,7 +36,25 @@ describe Api::V1::TimeSeriesValuesController, type: :controller do
       it 'lists all time series values' do
         get :index
         parsed_body = JSON.parse(response.body)
+        expect(parsed_body.length).to eq(15)
+      end
+
+      it 'filters time_series_values by model' do
+        get :index, params: {model: model_with_time_series.id}
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.length).to eq(5)
+      end
+
+      it 'filters time_series_values by scenario' do
+        get :index, params: {scenario: scenario.id}
+        parsed_body = JSON.parse(response.body)
         expect(parsed_body.length).to eq(10)
+      end
+
+      it 'filters time_series_values by indicator' do
+        get :index, params: {indicator: indicator.id}
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.length).to eq(15)
       end
     end
   end
