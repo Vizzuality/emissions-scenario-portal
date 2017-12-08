749c7f1aa1ce2983152643c685553dba82cf0cce
Kuba Kuźma <kuba@jah.pl>
2017-12-08 20:17:22 +0100

    updated error schema

--- spec/services/upload_notes_spec.rb
+++ spec/services/upload_notes_spec.rb
@@ -10,43 +10,61 @@ RSpec.describe UploadNotes, upload: :s3 do
       data: file
     )
   end
+  let!(:user) { create(:user, admin: true) }
+  let!(:model) do
+    create(:model, full_name: "Annual Energy Outlook 2017")
+  end
+  let!(:electricity) { create(:category, name: "Electricity") }
+  let!(:additions) do
+    create(
+      :category,
+      name: "Capacity additions by technology",
+      parent: electricity
+    )
+  end
+  let!(:coal) do
+    create(
+      :indicator,
+      category: electricity,
+      subcategory: additions,
+      name: "Coal w/o CCS"
+    )
+  end
+  let!(:gas) do
+    create(
+      :indicator,
+      category: electricity,
+      subcategory: additions,
+      name: "Gas w/o CCS"
+    )
+  end
 
   subject { UploadNotes.new(csv_upload).call }
 
   context 'when file correct' do
-    let!(:user) { create(:user, admin: true) }
-    let!(:model) do
-      create(:model, full_name: "Annual Energy Outlook 2017")
-    end
-    let!(:electricity) { create(:category, name: "Electricity") }
-    let!(:additions) do
-      create(
-        :category,
-        name: "Capacity additions by technology",
-        parent: electricity
-      )
-    end
-    let!(:coal) do
-      create(
-        :indicator,
-        category: electricity,
-        subcategory: additions,
-        name: "Coal w/o CCS"
-      )
-    end
-    let!(:gas) do
-      create(
-        :indicator,
-        category: electricity,
-        subcategory: additions,
-        name: "Gas w/o CCS"
-      )
-    end
     let(:file) do
       fixture_file_upload('files/notes-correct.csv')
     end
     it 'should have saved all rows' do
       expect { subject }.to change { Note.count }.by(2)
+    end
+    it 'should have correct message' do
+      subject
+      expect(csv_upload.message).to eq("2 of 2 rows saved.")
+    end
+  end
+
+  context 'when column is missing' do
+    let(:file) do
+      fixture_file_upload('files/notes-missing_column.csv')
+    end
+    it 'should have saved all rows' do
+      expect { subject }.to change { Note.count }.by(0)
+    end
+    it 'should have correct message' do
+      subject
+      expect(csv_upload.message).to eq("0 of 2 rows saved.")
+      puts csv_upload.errors_and_warnings.inspect
     end
   end
 end
