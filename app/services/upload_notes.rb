7920753690b17623091a3044fee2650471b47ba5
Kuba Kuźma <kuba@jah.pl>
2017-12-11 11:13:26 +0100

    added more specs

--- app/services/upload_notes.rb
+++ app/services/upload_notes.rb
@@ -1,41 +1,25 @@
 class UploadNotes
-  extend ActiveModel::Naming
-  attr_reader :csv_upload, :errors
+  include ActiveModel::Model
 
   HEADERS = %w[
     model_name esp_indicator_name unit_of_entry conversion_factor description
   ].freeze
 
+  attr_reader :csv_upload
+  validate :number_of_headers, :existence_of_headers
+
   def initialize(csv_upload)
     @csv_upload = csv_upload
-    @errors = ActiveModel::Errors.new(self)
   end
 
   def call
-    validate_number_of_headers
-    validate_headers
-
     ActiveRecord::Base.transaction do
-      notes = create_notes if errors.blank?
+      notes = create_notes if valid?
       update_csv_upload(notes || [])
     end
-
-    self
   end
 
   private
-
-  def read_attribute_for_validation(attribute)
-    send(attribute)
-  end
-
-  def self.human_attribute_name(attribute, options = {})
-    attribute
-  end
-
-  def self.lookup_ancestors
-    [self]
-  end
 
   def path
     @path ||= Paperclip.io_adapters.for(csv_upload.data).path
@@ -46,7 +30,7 @@ class UploadNotes
     @csv ||= CSV.open(path, 'r', headers: true, encoding: encoding).read
   end
 
-  def validate_number_of_headers
+  def number_of_headers
     return if HEADERS.size == csv.headers.size
     errors.add(
       :csv_upload,
@@ -62,7 +46,7 @@ class UploadNotes
     normalized if HEADERS.include?(normalized)
   end
 
-  def validate_headers
+  def existence_of_headers
     remaining_headers = HEADERS.dup
 
     csv.headers.each do |header|
@@ -88,12 +72,13 @@ class UploadNotes
   end
 
   def create_note(attributes)
-    Note.find_or_initialize_by(
-      model: Pundit.
+    model = Pundit.
         policy_scope(csv_upload.user, Model).
-        find_by_name(attributes['model_name']),
-      indicator: Indicator.find_by_name(attributes['esp_indicator_name'])
-    ).tap do |note|
+        find_by_name(attributes['model_name'])
+
+    indicator = Indicator.find_by_name(attributes['esp_indicator_name'])
+
+    Note.find_or_initialize_by(model: model, indicator: indicator).tap do |note|
       note.update(attributes.except('model_name', 'esp_indicator_name'))
     end
   end
@@ -132,9 +117,7 @@ class UploadNotes
       success: errors.present?,
       message: "#{notes.select(&:valid?).size} of #{csv.size} rows saved.",
       finished_at: Time.current,
-      errors_and_warnings: {
-        errors: errors.details[:csv_upload]
-      }
+      errors_and_warnings: {errors: errors.details[:csv_upload]}
     )
   end
 end
--- spec/services/upload_notes_spec.rb
+++ spec/services/upload_notes_spec.rb
@@ -52,6 +52,16 @@ RSpec.describe UploadNotes, upload: :s3 do
       subject
       expect(csv_upload.message).to eq("2 of 2 rows saved.")
     end
+    it 'overwrites the existing notes' do
+      subject
+      csv_upload = create(
+        :csv_upload,
+        user: user,
+        service_type: 'UploadNotes',
+        data: file
+      )
+      expect { UploadNotes.new(csv_upload).call }.not_to change { Note.count }
+    end
   end
 
   context 'when column is missing' do
@@ -64,7 +74,19 @@ RSpec.describe UploadNotes, upload: :s3 do
     it 'should have correct message' do
       subject
       expect(csv_upload.message).to eq("0 of 2 rows saved.")
-      puts csv_upload.errors_and_warnings.inspect
+    end
+  end
+
+  context 'when indicator is missing' do
+    let(:file) do
+      fixture_file_upload('files/notes-missing_indicator.csv')
+    end
+    it 'should have saved all rows' do
+      expect { subject }.to change { Note.count }.by(1)
+    end
+    it 'should have correct message' do
+      subject
+      expect(csv_upload.message).to eq("1 of 2 rows saved.")
     end
   end
 end
