f4a068caf3f456df6701fc46a1de05b42b4125da
Kuba Kuźma <kuba@jah.pl>
2017-12-20 10:43:19 +0100

    duplicated migration constants to avoid errors

--- db/migrate/20171124083731_move_indicators_to_notes.rb
+++ db/migrate/20171124083731_move_indicators_to_notes.rb
@@ -9,6 +9,15 @@ class MoveIndicatorsToNotes < ActiveRecord::Migration[5.1]
     "GW->EJ/yr" => "EJ/yr"
   }
 
+  CONVERSION_FACTORS = {
+    "TWh/yr->EJ/yr" => 0.0036,
+    "Mt CH4/yr->Mt CO2e/yr" => 25,
+    "kt N2O/yr->Mt CO2e/yr" => 0.001,
+    "EJ/yr->TWh/yr" => 277.8,
+    "billion US$2010/yr->billion US$2016/yr" => 1.1007,
+    "billion US$2012/yr->billion US$2016/yr" => 1.0454
+  }
+
   def change
     Indicator.where.not(model_id: nil).find_each do |indicator|
       # find an actual parent in theoretically one level deep structure
@@ -39,7 +48,7 @@ class MoveIndicatorsToNotes < ActiveRecord::Migration[5.1]
       else
         attributes[:unit_of_entry] = indicator.unit_of_entry
         attributes[:conversion_factor] =
-          AddMissingConversionFactors::CONVERSION_FACTORS[
+          CONVERSION_FACTORS[
             [attributes[:unit_of_entry], parent.unit].join('->')
           ]
 
--- db/migrate/20171124084629_assign_time_series_values_to_system_indicators.rb
+++ db/migrate/20171124084629_assign_time_series_values_to_system_indicators.rb
@@ -3,6 +3,20 @@ class AssignTimeSeriesValuesToSystemIndicators < ActiveRecord::Migration[5.1]
   class Indicator < ApplicationRecord; end
   class IncompatibleUnits < StandardError; end
 
+  CONVERSION_FACTORS = {
+    "TWh/yr->EJ/yr" => 0.0036,
+    "Mt CH4/yr->Mt CO2e/yr" => 25,
+    "kt N2O/yr->Mt CO2e/yr" => 0.001,
+    "EJ/yr->TWh/yr" => 277.8,
+    "billion US$2010/yr->billion US$2016/yr" => 1.1007,
+    "billion US$2012/yr->billion US$2016/yr" => 1.0454
+  }
+
+  UNIT_RESOLUTIONS = {
+    "GW->GW/yr" => "GW/yr",
+    "GW->EJ/yr" => "EJ/yr"
+  }
+
   def change
     Indicator.where.not(parent_id: nil).find_each do |indicator|
       # find an actual parent in theoretically one level deep structure
@@ -10,7 +24,7 @@ class AssignTimeSeriesValuesToSystemIndicators < ActiveRecord::Migration[5.1]
       parent = Indicator.find(parent.parent_id) while parent.parent_id.present?
 
       if indicator.unit != parent.unit
-        conversion_factor = AddMissingConversionFactors::CONVERSION_FACTORS[
+        conversion_factor = CONVERSION_FACTORS[
           [indicator.unit, parent.unit].join('->')
         ]
         if conversion_factor
@@ -18,7 +32,7 @@ class AssignTimeSeriesValuesToSystemIndicators < ActiveRecord::Migration[5.1]
             where(unit_of_entry: indicator.unit, indicator_id: indicator.id).
             update_all(['value = value * ?, unit_of_entry = ?', conversion_factor, parent.unit])
         else
-          unit = MoveIndicatorsToNotes::UNIT_RESOLUTIONS[
+          unit = UNIT_RESOLUTIONS[
             [indicator.unit, parent.unit].join('->')
           ]
 
