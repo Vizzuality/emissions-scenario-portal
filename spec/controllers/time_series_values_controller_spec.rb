02a80123e65ea3965125f2abaa441676be3ac732
Kuba Kuźma <kuba@jah.pl>
2018-01-10 17:47:08 +0100

    fixed csv generation to include only model specific data if given

--- spec/controllers/time_series_values_controller_spec.rb
+++ spec/controllers/time_series_values_controller_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe TimeSeriesValuesController, type: :controller do
     login_user
 
     describe 'GET index' do
-      let(:team_model) { create(:model, team: @user.team) }
-      let(:team_scenario) { create(:scenario, model: team_model) }
+      let(:team_model1) { create(:model, team: @user.team) }
+      let(:team_model2) { create(:model, team: @user.team) }
+      let(:team_scenario1) { create(:scenario, model: team_model1) }
+      let(:team_scenario2) { create(:scenario, model: team_model2) }
       let(:indicator) { create(:indicator) }
 
       it 'returns scenario time series file' do
@@ -14,11 +16,26 @@ RSpec.describe TimeSeriesValuesController, type: :controller do
 
         get(
           :index,
-          params: {model_id: team_model.id, scenario_id: team_scenario.id}
+          params: {model_id: team_model1.id, scenario_id: team_scenario.id}
         )
         expect(response.content_type).to eq('text/csv')
         expect(response.headers['Content-Disposition']).
           to eq('attachment; filename=scenario_time_series_data.csv')
+      end
+
+      it 'returns only model specific data' do
+        create(:time_series_value, scenario: team_scenario1, indicator: indicator)
+        create(:time_series_value, scenario: team_scenario2, indicator: indicator)
+
+        get(
+          :index,
+          params: {model_id: team_model1.id, indicator_id: indicator.id}
+        )
+        expect(response.content_type).to eq('text/csv')
+        expect(response.headers['Content-Disposition']).
+          to eq('attachment; filename=indicator_time_series_data.csv')
+
+        expect(response.body.lines.size).to eq(2)
       end
 
       it 'returns indicator time series file' do
