078e4789d108bf35fd4281cb6578770aacddefbf
Kuba Kuźma <kuba@jah.pl>
2018-01-25 11:53:43 +0100

    simplified controller

--- app/controllers/api/v1/time_series_values_controller.rb
+++ app/controllers/api/v1/time_series_values_controller.rb
@@ -4,14 +4,14 @@ module Api
       # rubocop:disable AbcSize
       def index
         values = TimeSeriesValue.includes(:indicator, :scenario).order(:year)
-        values = values.where(location_id: location_ids) if location_ids
-        values = values.where(scenario_id: scenario_ids) if scenario_ids
-        if model_ids
+        values = values.where(location_id: location_ids) if location_ids.present?
+        values = values.where(scenario_id: scenario_ids) if scenario_ids.present?
+        if model_ids.present?
           values = values.joins(:scenario).
             where(scenarios: {model_id: model_ids})
         end
 
-        values = values.where(indicator_id: indicator_ids) if indicator_ids
+        values = values.where(indicator_id: indicator_ids) if indicator_ids.present?
 
         render json: values
       end
@@ -20,35 +20,19 @@ module Api
       private
 
       def location_ids
-        if params[:location].blank?
-          nil
-        else
-          params[:location].split(',')
-        end
+        params[:location].to_s.split(',')
       end
 
       def scenario_ids
-        if params[:scenario].blank?
-          nil
-        else
-          params[:scenario].split(',')
-        end
+        params[:scenario].to_s.split(',')
       end
 
       def model_ids
-        if params[:model].blank?
-          nil
-        else
-          params[:model].split(',')
-        end
+        params[:model].to_s.split(',')
       end
 
       def indicator_ids
-        if params[:indicator].blank?
-          nil
-        else
-          params[:indicator].split(',')
-        end
+        params[:indicator].to_s.split(',')
       end
     end
   end
