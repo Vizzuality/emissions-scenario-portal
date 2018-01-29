3354ce680bde088c6760cb1d979b778a791156be
Kuba Kuźma <kuba@kubakuzma.com>
2018-01-29 16:29:53 +0100

    fixed once again

--- app/templates/time_series_values_upload_template.rb
+++ app/templates/time_series_values_upload_template.rb
@@ -1,8 +1,9 @@
 class TimeSeriesValuesUploadTemplate
   def export
     years = %w(2005 2010 2020 2030 2040 2050 2060 2070 2080 2090 2100)
+    headers = UploadTimeSeriesValues.headers.values
     CSV.generate do |csv|
-      csv << UploadTimeSeriesValues.headers.values + years
+      csv << headers + years
       csv << [nil] * (headers.length + years.length)
     end
   end
