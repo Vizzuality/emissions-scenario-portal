f0e5938fb0a6ea001664f7471ddb46ced16039fb
Simao Belchior <simaobelchior@gmail.com>
2018-02-23 19:13:51 +0000

    Updates metadata fields for scenarios
    Adds, renames, and removes fields as requested by the client.
    Updates the template and the various references to these fields.

--- app/serializers/api/v1/scenario_serializer.rb
+++ app/serializers/api/v1/scenario_serializer.rb
@@ -2,39 +2,21 @@ module Api
   module V1
     class ScenarioSerializer < ActiveModel::Serializer
       attribute :id
-      attribute :name
       attribute :model_abbreviation
+      attribute :name
+      attribute :year
       attribute :category
-      attribute :description
-      attribute :geographic_coverage_country
-      attribute :sectoral_coverage
-      attribute :gas_and_pollutant_coverage
-      attribute :policy_coverage
-      attribute :policy_coverage_detailed
-      attribute :technology_coverage
-      attribute :technology_coverage_detailed
-      attribute :energy_resource_coverage
-      attribute :time_horizon
-      attribute :time_step
-      attribute :climate_target_type
-      attribute :large_scale_bioccs
-      attribute :technology_assumptions
-      attribute :gdp_assumptions
-      attribute :population_assumptions
-      attribute :discount_rates
-      attribute :emission_factors
-      attribute :global_warming_potentials
-      attribute :policy_cut_off_year_for_baseline
-      attribute :literature_reference
       attribute :purpose_or_objective
-      attribute :key_usage
-      attribute :project
-      attribute :climate_target_detailed
-      attribute :climate_target_date
-      attribute :overshoot
-      attribute :other_target_type
+      attribute :description
+      attribute :reference
+      attribute :url
+      attribute :policy_coverage
+      attribute :technology_coverage
+      attribute :socioeconomics
+      attribute :climate_target
       attribute :other_target
       attribute :burden_sharing
+      attribute :discount_rates
 
       belongs_to :model
       has_many :indicator_ids
