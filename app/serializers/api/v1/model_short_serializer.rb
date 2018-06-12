0a15b946d5f588ec8bad7b9bddaf9b7f75e6e28c
Simao Belchior <simaobelchior@gmail.com>
2018-06-12 08:37:51 +0100

    Include logo in model serializer

--- app/serializers/api/v1/model_short_serializer.rb
+++ app/serializers/api/v1/model_short_serializer.rb
@@ -3,6 +3,13 @@ module Api
     class ModelShortSerializer < ActiveModel::Serializer
       attribute :id
       attribute :full_name, key: :name
+      attribute :logo
+
+      def logo
+        object.logo_file_name ?
+          object.logo.url(:original) :
+          object.team && object.team.image_file_name && object.team.image.url(:original)
+      end
     end
   end
 end
